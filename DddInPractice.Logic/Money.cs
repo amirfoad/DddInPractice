502bbdf7c7fcb109772ee9d51d1a5e07c39f150c
amirfoad <amirfoadahmadi@gmail.com>
2023-09-08 02:44:41 +0330

    inhertiance money from valueObject

--- DddInPractice.Logic/Money.cs
+++ DddInPractice.Logic/Money.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace DddInPractice.Logic
 {
-	public sealed class Money
+	public sealed class Money :ValueObject<Money>
 	{
         public int OneCentCount { get; private set; }
         public int TenCentCount { get; private set; }
@@ -35,6 +35,30 @@ namespace DddInPractice.Logic
                 money1.FiveDollarCount + money2.FiveDollarCount,
                 money1.TwentyDollarCount + money2.TwentyDollarCount);
             return sum;
+        }
+
+        protected override bool EqualsCore(Money other)
+        {
+            return OneCentCount == other.OneCentCount
+                && TenCentCount == other.TenCentCount
+                && QuarterCentCount == other.QuarterCentCount
+                && OneDollarCount == other.OneDollarCount
+                && FiveDollarCount == other.FiveDollarCount
+                && TwentyDollarCount == other.TwentyDollarCount;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                int hashcode = OneCentCount;
+                hashcode = (hashcode * 397) ^ TenCentCount;
+                hashcode = (hashcode * 397) ^ QuarterCentCount;
+                hashcode = (hashcode * 397) ^ OneDollarCount;
+                hashcode = (hashcode * 397) ^ FiveDollarCount;
+                hashcode = (hashcode * 397) ^ TwentyDollarCount;
+                return hashcode;
+            }
         }
     }
 }
