b7ba2712e7439d1071bb9979503af8293025e249
amirfoad <amirfoadahmadi@gmail.com>
2023-09-08 23:24:46 +0330

    implement snack machine tests

--- DddInPractice.Logic/SnackMachine.cs
+++ DddInPractice.Logic/SnackMachine.cs
@@ -1,34 +1,41 @@
 ﻿using System;
+using static DddInPractice.Logic.Money;
+
 namespace DddInPractice.Logic
 {
-    public sealed class SnackMachine
+    public sealed class SnackMachine :Entity
     {
         /// <summary>
         /// The amount of money machine has
         /// </summary>
-        public Money MoneyInside { get; private set; }
+        public Money MoneyInside { get; private set; } = None;
 
 
         /// <summary>
         /// The amount of money user inserted
         /// </summary>
-        public Money MoneyInTransaction { get; private set; }
+        public Money MoneyInTransaction { get; private set; } = None;
+
 
         public void InsertMoney(Money money)
         {
+            Money[] coinAndNotes = { Cent, TenCent, Quarter, Dollar, FiveDollar, TwentyDollar };
+            if (coinAndNotes.Contains(money))
+                throw new InvalidOperationException();
+
             MoneyInTransaction += money;
         }
 
         public void ReturnMoney()
         {
-            //MoneyInTransaction = 0;
+            MoneyInTransaction = None;
         }
 
         public void BuySnack()
         {
             MoneyInside += MoneyInTransaction;
 
-            //MoneyInTransaction = 0;
+            MoneyInTransaction = None;
         }
     }
 }
